584787c4f00fd6b7e0be15efceed6cd71f4d332e
s444478 <wojtek.sek@onet.pl>
2019-10-07 12:56:54 +0200

    moj pierwszy commit

--- src/main/java/pracownia/introduction/HelloWorld.java
+++ src/main/java/pracownia/introduction/HelloWorld.java
@@ -1,8 +1,19 @@
 package pracownia.introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
     public static void main(String [ ] args) {
         System.out.println("Hello World");
+
+        Logger log = Logger.getLogger("sd");
+
+        log.info("adaasdas");
+        log.error("sdadas");
+
+
     }
+
+
 }
